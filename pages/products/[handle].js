bb03ced6e87a847b48051a7260c438340f78978c
Nick Richmond <get@nickrichmond.dev>
2020-08-04 18:12:40 -0400

    Create PDP for single product at /products/alonso-shoes

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -1,28 +1,41 @@
 import NacelleClient from '@nacelle/client-js-sdk';
 const Product = ({ product }) => {
-  return <div>Test</div>;
+  return <pre>{JSON.stringify(product)}</pre>;
 };
 
-async function getStaticPaths() {
-  const settings = {
-    id: 'rude-parrot-iBiKZQDPOa',
-    token: '8638f8ca-4934-436e-80bd-851a710abc04',
-    locale: 'en-us',
-    staticBasePath:
-      'https://nacelle-demo-sites-git-feature-dash-dash.nacelle.vercel.app/',
-    nacelleEndpoint: 'https://hailfrequency.com/v2/graphql'
-  };
+export default Product
 
-  const client = new NacelleClient(settings);
-  const space = await client.data.space();
-  // const product = await client.data.product({ handle: 'tanja-heel-sandals' });
-  const product = await client.data.products({
+const settings = {
+  id: 'rude-parrot-iBiKZQDPOa',
+  token: '8638f8ca-4934-436e-80bd-851a710abc04',
+  locale: 'en-us',
+  staticBasePath:
+    'https://nacelle-demo-sites-git-feature-dash-dash.nacelle.vercel.app/',
+  nacelleEndpoint: 'https://hailfrequency.com/v2/graphql'
+};
+
+const client = new NacelleClient(settings);
+
+export async function getStaticPaths() {
+  const products = await client.data.products({
     handles: ['alonso-shoes']
   });
 
-  console.log(product);
+  return {
+    paths: products.map(product => {
+      const { handle } = product
+      return { params: { handle }}
+    }),
+    fallback: false // See the "fallback" section below
+  };
 }
 
-// async function getStaticProps() {
-//   return { props: {} };
-// }
+export async function getStaticProps({ params }) {
+  const product = await client.data.product({
+    handle: params.handle
+  });
+  return {
+    props: { product }, // will be passed to the page component as props
+  }
+}
+
