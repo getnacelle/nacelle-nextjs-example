fcb45e4b486c9ca79fbd6bc838ab4436958196f7
Nick Richmond <get@nickrichmond.dev>
2020-08-05 00:31:53 -0400

    Configure routes for pages

---
+++ pages/pages/[handle].js
@@ -0,0 +1,32 @@
+import $nacelle from 'services/nacelle.js'
+
+const Page = ({ page }) => {
+  return <pre>{JSON.stringify(page)}</pre>;
+};
+
+export default Page
+
+export async function getStaticPaths() {
+  try {
+    const allContent = await $nacelle.data.allContent()
+    const pages = allContent.filter(entry => entry.type === "page")
+    return {
+      paths: pages.map(page => {
+        const { handle } = page
+        return { params: { handle } }
+      }),
+      fallback: false // See the "fallback" section below
+    };
+  } catch (err) {
+    console.error(`Error fetching page:\n${err}`)
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const page = await $nacelle.data.page({
+    handle: params.handle
+  });
+  return {
+    props: { page }, // will be passed to the page component as props
+  }
+}
