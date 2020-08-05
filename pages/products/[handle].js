95e428d9c0a6b6b800a9d9d914a402e5f91dc40d
Nick Richmond <get@nickrichmond.dev>
2020-08-04 22:49:06 -0400

    Configure PDP with route payloads

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -1,4 +1,5 @@
 import NacelleClient from '@nacelle/client-js-sdk';
+
 const Product = ({ product }) => {
   return <pre>{JSON.stringify(product)}</pre>;
 };
@@ -10,24 +11,25 @@ const settings = {
   token: '8638f8ca-4934-436e-80bd-851a710abc04',
   locale: 'en-us',
   staticBasePath:
-    'https://nacelle-demo-sites-git-feature-dash-dash.nacelle.vercel.app/',
+    'https://nacelle-demo-store-data.s3.amazonaws.com/',
   nacelleEndpoint: 'https://hailfrequency.com/v2/graphql'
 };
 
 const client = new NacelleClient(settings);
 
 export async function getStaticPaths() {
-  const products = await client.data.products({
-    handles: ['alonso-shoes']
-  });
-
-  return {
-    paths: products.map(product => {
-      const { handle } = product
-      return { params: { handle }}
-    }),
-    fallback: false // See the "fallback" section below
-  };
+  try {
+    const products = await client.data.allProducts()
+    return {
+      paths: products.map(product => {
+        const { handle } = product
+        return { params: { handle }}
+      }),
+      fallback: false // See the "fallback" section below
+    };
+  } catch(err) {
+    console.error(`Error fetching products on homepage:\n${err}`)
+  }  
 }
 
 export async function getStaticProps({ params }) {
