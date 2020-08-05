5cd8eb32d95c885288f7058acfaf322bf2f3354f
Nick Richmond <get@nickrichmond.dev>
2020-08-05 14:59:31 -0400

    Export components for easy imports via 'components/name'

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as ProductCard } from './product-card'
+export { default as ProductGallery } from './product-gallery'
+export { default as HeroBanner } from './hero-banner'
+export { default as Sections } from './sections'
