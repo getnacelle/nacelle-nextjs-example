78b49e55ea91d499a7b28226da333df23c6e5bcd
Nick Richmond <get@nickrichmond.dev>
2020-08-05 20:24:20 -0400

    Add components to list of named imports in 'components' dir

--- components/index.js
+++ components/index.js
@@ -3,3 +3,5 @@ export { default as ProductGallery } from './product-gallery';
 export { default as HeroBanner } from './hero-banner';
 export { default as Sections } from './sections';
 export { default as Layout } from './layout';
+export { default as Header } from './header';
+export { default as Cart } from './cart';
