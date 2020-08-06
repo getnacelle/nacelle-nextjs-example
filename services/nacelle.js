eef7c8754bda45c8be8f2f645be11ac9f694216f
Nick Richmond <get@nickrichmond.dev>
2020-08-06 18:00:07 -0400

    Add & configure Contentful Preview Connector

--- services/nacelle.js
+++ services/nacelle.js
@@ -1,4 +1,5 @@
 import NacelleClient from '@nacelle/client-js-sdk';
+import NacelleContentfulPreviewConnector from '@nacelle/contentful-preview-connector';
 
 const settings = {
   id: process.env.NACELLE_SPACE_ID,
@@ -38,8 +39,31 @@ const nacelleClient = {
     article: (params) => clientCMS.data.article(params),
     articles: (params) => clientCMS.data.articles(params),
     blog: (params) => clientCMS.data.blog(params),
-    blogPage: (params) => clientCMS.data.blogPage(params)
+    blogPage: (params) => clientCMS.data.blogPage(params),
+    update: (params) => clientCMS.data.update(params)
   }
 };
+
+if (
+  (process.env.NACELLE_PREVIEW_MODE ||
+    process.env.VERCEL_GITHUB_COMMIT_REF === 'preview') &&
+  process.env.CONTENTFUL_PREVIEW_SPACE_ID &&
+  process.env.CONTENTFUL_PREVIEW_API_TOKEN
+) {
+  console.log('RUNNING NACELLE IN PREVIEW MODE');
+  // Initialize the Preview Connector
+  // Note: the Contentful Preview API token is not the same as your Content Delivery API token
+  const previewConnector = new NacelleContentfulPreviewConnector({
+    contentfulSpaceID: process.env.CONTENTFUL_PREVIEW_SPACE_ID,
+    contentfulToken: process.env.CONTENTFUL_PREVIEW_API_TOKEN
+  });
+
+  nacelleClient.data.content = (params) => previewConnector.content(params);
+  nacelleClient.data.pages = (params) => previewConnector.pages(params);
+  nacelleClient.data.page = (params) => previewConnector.page(params);
+  nacelleClient.data.article = (params) => previewConnector.article(params);
+  nacelleClient.data.articles = (params) => previewConnector.articles(params);
+  nacelleClient.data.blog = (params) => previewConnector.blog(params);
+}
 
 export default nacelleClient;
