67501606785fc5811d1a9caa1eb993c781bab236
Nick Richmond <get@nickrichmond.dev>
2020-08-11 10:54:42 -0400

    Use optional chaining for added flexibility

--- components/sections.js
+++ components/sections.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { HeroBanner, SideBySide } from 'components';
 
-const CollectionGrid = ({ fields }) => <h1>COLLECTION GRID</h1>;
-
 export default function Sections({ sections, options }) {
   return (
     Array.isArray(sections) &&
     sections.map((section) => {
-      const type = section.sys.contentType.sys.id;
+      const type = section?.sys?.contentType?.sys?.id;
       switch (type) {
         case 'heroBanner': {
           return (
@@ -23,11 +21,7 @@ export default function Sections({ sections, options }) {
         case 'sideBySide': {
           return <SideBySide fields={section.fields} key={section.sys.id} />;
         }
-        case 'collectionGrid': {
-          return (
-            <CollectionGrid fields={section.fields} key={section.sys.id} />
-          );
-        }
+        // ...add additional cases for whichever content types are needed
       }
     })
   );
