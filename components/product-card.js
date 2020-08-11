4fdef040fa6feae6cde893753592f3b359f27cfb
Nick Richmond <get@nickrichmond.dev>
2020-08-11 10:56:33 -0400

    Remove <WhenLoading> component

--- components/product-card.js
+++ components/product-card.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useAddToCart, useFormatPrice } from 'hooks';
-import { Image, WhenVisible } from 'components';
+import { Image } from 'components';
 import * as styles from 'styles/products.styles';
 
 const LinkPDP = ({ linkToPDP, children }) => {
@@ -41,51 +41,47 @@ const ProductCard = ({ product, linkToPDP }) => {
   const decrementQty = () => setQuantity((qty) => (qty > 0 ? qty - 1 : 0));
 
   return (
-    <WhenVisible verticalThreshold={800}>
-      <article css={{ margin: '0 2em' }}>
-        <div css={{ ...styles.column }}>
-          <LinkPDP linkToPDP={linkToPDP ? `/products/${product.handle}` : null}>
-            <Image
-              src={product.featuredMedia.src}
-              alt={product.featuredMedia.altText}
-              width="320"
-              format={['webp', 'jpg']}
-              styles={{
-                maxWidth: '100%',
-                minHeight: '100px',
-                height: 'auto',
-                width: '100%'
-              }}
-            />
-          </LinkPDP>
+    <article css={{ margin: '0 2em' }}>
+      <div css={{ ...styles.column }}>
+        <LinkPDP linkToPDP={linkToPDP ? `/products/${product.handle}` : null}>
+          <Image
+            src={product.featuredMedia.src}
+            alt={product.featuredMedia.altText}
+            width="320"
+            format={['webp', 'jpg']}
+            styles={{
+              maxWidth: '100%',
+              minHeight: '100px',
+              height: 'auto',
+              width: '100%'
+            }}
+          />
+        </LinkPDP>
 
-          <div css={styles.column}>
-            <LinkPDP
-              linkToPDP={linkToPDP ? `/products/${product.handle}` : null}
-            >
-              <h3 css={styles.productTitle}>{product.title}</h3>
-            </LinkPDP>
-            <span css={styles.productPrice}>{price}</span>
-            <div css={styles.productInteractLayout}>
-              <div css={styles.counterLayout}>
-                <span css={styles.quantity}>{quantity}</span>
-                <div css={styles.counterSwitchLayout}>
-                  <button css={styles.counterSwitch} onClick={incrementQty}>
-                    +
-                  </button>
-                  <button css={styles.counterSwitch} onClick={decrementQty}>
-                    -
-                  </button>
-                </div>
+        <div css={styles.column}>
+          <LinkPDP linkToPDP={linkToPDP ? `/products/${product.handle}` : null}>
+            <h3 css={styles.productTitle}>{product.title}</h3>
+          </LinkPDP>
+          <span css={styles.productPrice}>{price}</span>
+          <div css={styles.productInteractLayout}>
+            <div css={styles.counterLayout}>
+              <span css={styles.quantity}>{quantity}</span>
+              <div css={styles.counterSwitchLayout}>
+                <button css={styles.counterSwitch} onClick={incrementQty}>
+                  +
+                </button>
+                <button css={styles.counterSwitch} onClick={decrementQty}>
+                  -
+                </button>
               </div>
-              <button css={styles.addToCartButton} onClick={addItemToCart}>
-                ADD TO CART
-              </button>
             </div>
+            <button css={styles.addToCartButton} onClick={addItemToCart}>
+              ADD TO CART
+            </button>
           </div>
         </div>
-      </article>
-    </WhenVisible>
+      </div>
+    </article>
   );
 };
 
